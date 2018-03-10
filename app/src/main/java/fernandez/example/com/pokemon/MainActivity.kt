2b2c2281c3ae244a5430f7983380dd5608b1ee9b
kennethfernandez <kenfer144@gmail.com>
2018-03-10 08:11:22 +0800

    last commit

--- app/src/main/java/fernandez/example/com/pokemon/MainActivity.kt
+++ app/src/main/java/fernandez/example/com/pokemon/MainActivity.kt
@@ -1,7 +1,7 @@
 package fernandez.example.com.pokemon
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.os.Bundlegit
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
